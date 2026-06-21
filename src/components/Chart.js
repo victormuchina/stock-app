3045998c39daf9c3bfff1b52e7129cb194feb096
Victor Kamutu <victor.kamutu@strathmore.edu>
2026-06-21 13:57:46 +0300

    Complete Stock-app build

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,1 +1,37 @@
 // Write your Charts component here
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const LineChart = ({ chartData }) => {
+  return (
+    <>
+      <Line
+        data={chartData}
+        options={{
+          scales: {
+            x: {
+              ticks: {
+                autoSkip: true,
+                maxTicksLimit: 10,
+                maxRotation: 0,
+              }
+            }
+          }
+        }}
+      />
+    </>
+  );
+};
+
+export default LineChart;
--- src/pages/Stocks.js
+++ src/pages/Stocks.js
@@ -1,11 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import LineChart from '../components/Chart';
+import axios from 'axios';
 
-const Stocks = () => {    
-    return (
-        <>
-            Stock Details Page
-        </>
-    )
+const Stocks = () => {
+  const [chartData, setChartData] = useState({});
+  const [summary, setSummary] = useState(null);
+  const [flag, setFlag] = useState(null);
+const { symbol } = useParams();  const apiKey = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    const fetchTrend = async () => {
+      try {
+        const options = {
+          method: 'GET',
+url: `https://yfapi.net/v8/finance/chart/${symbol}?range=1mo&interval=1d`,          headers: {
+            'x-api-key': apiKey
+          }
+        };
+
+        const response = await axios.request(options);
+        setChartData({
+          labels: response.data.chart.result[0].timestamp.map((elem) =>
+            new Date(elem * 1000).toLocaleDateString("en-US")
+          ),
+          datasets: [{
+            label: 'Changes Reflected Daily',
+            data: response.data.chart.result[0].indicators.quote[0].close,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderWidth: 5
+          }]
+        });
+        setSummary(response.data.chart.result[0].meta);
+        setFlag(response.data.chart.result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchTrend();
+  }, [apiKey, symbol]);
+
+  return (
+    <>
+      {
+        !flag ? (
+          <div className="container">
+            <div className="row">
+              <div className="card rounded shadow-lg m-2 chart-row">
+                <p className="error-message" style={{ padding: "5px" }}>Loading Data...</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="stock-row">
+            <div className="stock-column-data">
+              {
+                summary ? (
+                  <>
+                    <h2>
+                      <span style={{ color: "black" }}>{summary.exchangeName} ({summary.symbol})</span> ${summary.regularMarketPrice}
+                    </h2>
+                    <div className="stock-headings">
+                      <h4><span className="summary-span">Exchange Name:</span><span style={{ color: "black" }}>{" "}{summary.exchangeName}</span></h4>
+                      <br/>
+                      <h4><span className="summary-span">Instrument Type:</span><span style={{ color: "black" }}>{" "}{summary.instrumentType}</span></h4>
+                      <br/>
+                      <h4><span className="summary-span">Previous Close Price:</span><span style={{ color: "black" }}>{" "}${summary.chartPreviousClose}</span></h4>
+                      <br/>
+                      <h4><span className="summary-span">Current Price:</span><span style={{ color: "black" }}>{" "}${summary.regularMarketPrice}</span></h4>
+                    </div>
+                  </>
+                ) : (
+                  <div>Loading...</div>
+                )
+              }
+            </div>
+            <div className="stock-column-chart">
+              <LineChart chartData={chartData} />
+            </div>
+          </div>
+        )
+      }
+    </>
+  )
 }
 
 export default Stocks;
